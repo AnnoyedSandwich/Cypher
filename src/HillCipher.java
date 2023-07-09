e77aa07b352c5f1a78f780aca4e7de73194236b0
AnnoyedSandwich <41872159+AnnoyedSandwich@users.noreply.github.com>
2023-07-09 04:03:31 +0200

    PolygramCypher

---
+++ src/HillCipher.java
@@ -0,0 +1,63 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+//Hill Cipher, letteres correspond to numbers based on ascii code
+//n = 2;
+//TODO: make it work for n = x;
+
+public class HillCipher {
+    static Scanner sc = new Scanner(System.in);
+    public static StringBuilder HillEncrypted(String input){
+        int[][] keyMatrix = getKey();
+        int[][] inputMatrix = new int[2][1];
+        input = input.toUpperCase();
+        if (input.length() % 2 != 0){
+            input += "Z";
+        }
+        String[] inputArray = splitN(input);
+        //for loop über inputArray teile, in jedem loop einmal die 2x1 matrix mit key multiplizieren
+        StringBuilder encryptedString = new StringBuilder();
+        for (int i=0; i<inputArray.length; i++){
+            inputMatrix[0][0] = ((int) inputArray[i].charAt(0))-65;
+            inputMatrix[1][0] = ((int) inputArray[i].charAt(1))-65;
+            encryptedString.append(encrypt(inputMatrix, keyMatrix));
+        }
+
+        return encryptedString;
+    }
+    private static int[][] getKey(){
+        String key;
+        int[][] keyMatrix = new int[2][2];
+        do {
+            System.out.println("Enter key - 4 words");
+            key = sc.nextLine();
+        }while (key.length() != 4);
+        int k=0;
+        for (int i = 0; i < 2; i++) {
+            for (int j = 0; j < 2; j++) {
+                keyMatrix[i][j] = ((int) key.charAt(k))-97;
+                k++;
+            }
+        }
+        return keyMatrix;
+    }
+
+    private static String[] splitN(String input){
+            return input.split("(?<=\\G.{2})");
+        }
+
+    private static StringBuilder encrypt(int[][] inputMatrix, int[][] keyMatrix){
+        //multiplication
+        int firstNumber = inputMatrix[0][0]*keyMatrix[0][0] + inputMatrix[1][0]*keyMatrix[1][0];
+        int secondNumber = inputMatrix[0][0]*keyMatrix[0][1] + inputMatrix[1][0]*keyMatrix[1][1];
+        //mod 26 und in char umwandeln
+        firstNumber = firstNumber % 26;
+        secondNumber = secondNumber % 26;
+        char firstChar = (char) (firstNumber + 65);
+        char secondChar = (char) (secondNumber + 65);
+        return new StringBuilder().append(firstChar).append(secondChar);
+    }
+}
+
+
+
--- src/Main.java
+++ src/Main.java
@@ -5,12 +5,15 @@ public class Main {
     static Scanner sc = new Scanner(System.in);
 
     public static void main(String[] args) {
-        System.out.println("what encyption would you like to use?");
-        System.out.println("0. Exit");
-        System.out.println("1. Caesar Cipher");
-        System.out.println("2. MonoAlphabetic");
         int i;
         do {
+            System.out.println("------------------------------------");
+            System.out.println("what encyption would you like to use?");
+            System.out.println("0. Exit");
+            System.out.println("1. Caesar Cipher");
+            System.out.println("2. MonoAlphabetic");
+            System.out.printf("3. Polygram Cipher%n%n");
+            System.out.println("------------------------------------");
             i = sc.nextInt();
             sc.nextLine();
             switch (i) {
@@ -27,6 +30,11 @@ public class Main {
                     System.out.println("Enter String");
                     String input = sc.nextLine();
                     System.out.println("the decrypted String is: " + MonoAlphabetic.MAEncypted(input));
+                }
+                case 3 -> {
+                    System.out.println("Enter String");
+                    String input = sc.nextLine();
+                    System.out.println("the encrypted String is: " + HillCipher.HillEncrypted(input));
                 }
                 default -> System.out.println("Invalid input");
             }
